d6010184542776ad1b2ca2367fbde6efde74206a
jhadobe <jhadobe@users.noreply.github.com>
2024-02-16 20:18:49 +0000

    Deploying to gh-pages from @ AdobeDocs/commerce-extensibility@3ac3d6ab9fc2b536c12e95a0fa56c71da22e6397 🚀

---
+++ component---src-pages-app-development-starter-kit-index-md-bd621de25c714a813d57.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcommerce_extensibility=self.webpackChunkcommerce_extensibility||[]).push([[534],{89742:function(t,n,e){e.r(n),e.d(n,{_frontmatter:function(){return m},default:function(){return p}});var o=e(87462),c=e(45987),r=(e(35776),e(3905)),u=e(91515);const i=["components"],m={},s={_frontmatter:m},f=u.Z;function p(t){let{components:n}=t,e=(0,c.Z)(t,i);return(0,r.mdx)(f,(0,o.Z)({},s,e,{components:n,mdxType:"MDXLayout"}))}p.isMDXComponent=!0}}]);
+//# sourceMappingURL=component---src-pages-app-development-starter-kit-index-md-bd621de25c714a813d57.js.map
